0669566555de04dae722f450e5d26a72aa72ebc6
sharyu <sharayumarwadi11@gmail.com>
2024-03-17 20:24:15 +0530

    Formik added to signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 
 function App() {
+    const router = createBrowserRouter([
+        {
+            path: "/signup",
+            element: <SignUp />
+        },
+        {
+            path: "/login",
+            element: <Login />
+        }
+    ])
     return (
         <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/signup' element={<SignUp />} />
-                    <Route path='/login' element={<Login />} />
-                </Routes>
-            </BrowserRouter>
+            <RouterProvider router={router} />
         </div>
     );
 }
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,51 +1,88 @@
+import { Field, Formik, Form } from "formik"
 import { Link } from "react-router-dom"
 import "./SignUp.css"
+
+
+interface values {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phnNo: string;
+    company: string;
+    password: string;
+}
 const SignUp = () => {
+
+    const initialValues: values = {
+        firstName: "",
+        lastName: "",
+        email: "",
+        phnNo: "",
+        company: "default",
+        password: ""
+    }
     return (
         <div className="bg-grey-lighter min-h-screen flex flex-col">
             <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
                 <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
                     <h1 className="mb-8 text-3xl text-center">Sign up</h1>
-                    <input
-                        type="text"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="fullname"
-                        placeholder="First Name" />
-                    <input
-                        type="text"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="fullname"
-                        placeholder="Last Name" />
-                    <input
-                        type="text"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="email"
-                        placeholder="Email" />
-                    <input
-                        type="text"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="number"
-                        placeholder="Phone Number" />
-                    <select id="company" className="bg-transparent block border border-grey-light w-full p-3 rounded mb-4 text-gray-400">
-                        <option value="" disabled selected hidden>Select Company</option>
-                        <option value="Jsoh Software Pvt Ltd">Josh Software Pvt Ltd</option>
-                        <option value="Dhruva">Dhruva</option>
-                        <option value="Pattern">Pattern</option>
-                    </select>
-                    <input
-                        type="password"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="password"
-                        placeholder="Password" />
-                    <input
-                        type="password"
-                        className="block border border-grey-light w-full p-3 rounded mb-4"
-                        name="confirm_password"
-                        placeholder="Confirm Password" />
-                    <button
-                        type="submit"
-                        className="sign-up-btn w-full text-center py-3 rounded text-white hover:bg-green-dark focus:outline-none my-1"
-                    >Create Account</button>
+                    <Formik
+                        initialValues={initialValues}
+                        onSubmit={(values) => {
+                            console.log(values)
+                        }}
+                    >
+                        <Form>
+                            <Field
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="firstName"
+                                placeholder="First Name" />
+                            <Field
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="lastName"
+                                placeholder="Last Name" />
+                            <Field
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="email"
+                                placeholder="Email" />
+                            <Field
+                                type="text"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="phnNo"
+                                placeholder="Phone Number" />
+                            <Field
+                                as="select"
+                                name="company"
+                                className="bg-transparent block border border-grey-light w-full p-3 rounded mb-4 text-gray-400"
+                            >
+                                <option value="default" label="Select a company">
+                                    Select a company
+                                </option>
+                                <option value="Josh">
+                                    Josh
+                                </option>
+                                <option value="Pattern">
+                                    Pattern
+                                </option>
+                                <option value="Dhruva">
+                                    Dhruva
+                                </option>
+                            </Field>
+                            <Field
+                                type="password"
+                                className="block border border-grey-light w-full p-3 rounded mb-4"
+                                name="password"
+                                placeholder="Password" />
+                            <button
+                                id="submit"
+                                type="submit"
+                                className="sign-up-btn w-full text-center py-3 rounded text-white hover:bg-green-dark focus:outline-none my-1"
+                            >Create Account</button>
+                        </Form>
+                    </Formik>
 
                 </div>
 
@@ -60,3 +97,5 @@ const SignUp = () => {
     )
 }
 export default SignUp
+
+
